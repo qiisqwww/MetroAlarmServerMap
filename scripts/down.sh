1813e7899a79159ee951779a0702a8a814232ccb
qiisqwww <qiisqwww@gmail.com>
2024-07-05 20:56:32 +0300

    renamed project

--- scripts/down.sh
+++ scripts/down.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker-compose -f /var/www/ServerMetroAlarm/docker/docker-compose.yml down
+sudo docker-compose -f /var/www/MetroAlarmServer/docker/docker-compose.yml down
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker-compose -f /var/www/ServerMetroAlarm/docker/docker-compose.yml up --build -d
+sudo docker-compose -f /var/www/MetroAlarmServer/docker/docker-compose.yml up --build -d
